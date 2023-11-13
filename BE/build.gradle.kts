1ee71fb06d5d3182c656f20b56e3b13eb1a95b19
mae <ovoa22@gmail.com>
2023-11-13 14:18:46 +0900

    Update build.gradle.kts

--- BE/build.gradle.kts
+++ BE/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.boot:spring-boot-starter-security")
 
 	// OpenFeign
 	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
