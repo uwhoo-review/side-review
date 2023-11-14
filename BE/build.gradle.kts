761cf4dea6bdafea29b0c9d495b7488e822c2438
yekim <duddms9811@naver.com>
2023-11-14 17:56:01 +0900

    java 버전 변경, kotlin serializer 추가

--- BE/build.gradle.kts
+++ BE/build.gradle.kts
@@ -1,74 +1,78 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.7.16"
-	id("io.spring.dependency-management") version "1.0.15.RELEASE"
-	kotlin("jvm") version "1.9.0"
-	kotlin("plugin.spring") version "1.6.21"
-	kotlin("plugin.jpa") version "1.6.21"
+    id("org.springframework.boot") version "2.7.16"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.spring") version "1.6.21"
+    kotlin("plugin.jpa") version "1.6.21"
+    kotlin("plugin.serialization") version "1.9.0"
 }
 group = "com.sideReview"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 repositories {
-	mavenCentral()
-	maven("https://maven.tryformation.com/releases") {
-		content {
-			includeGroup("com.jillesvangurp")
-		}
-	}
+    mavenCentral()
+    maven("https://maven.tryformation.com/releases") {
+        content {
+            includeGroup("com.jillesvangurp")
+        }
+    }
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-security")
 
 
-	// OpenFeign
-	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+    // OpenFeign
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 
-	// AWS OpenSearch Client library
-	implementation("com.jillesvangurp:search-client:2.1.4")
+    // AWS OpenSearch Client library
+    implementation("com.jillesvangurp:search-client:2.1.4")
+
+    // serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 
 
-	compileOnly("org.projectlombok:lombok")
-	compileOnly("com.google.api-client:google-api-client:1.23.0")
-	compileOnly("com.google.oauth-client:google-oauth-client-jetty:1.23.0")
-	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    compileOnly("org.projectlombok:lombok")
+    compileOnly("com.google.api-client:google-api-client:1.23.0")
+    compileOnly("com.google.oauth-client:google-oauth-client-jetty:1.23.0")
+    runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 dependencyManagement {
-	imports {
-		mavenBom("org.springframework.cloud:spring-cloud-dependencies:2021.0.8")
-	}
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2021.0.8")
+    }
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "11"
-	}
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 tasks.getByName<Jar>("jar") {
-	enabled = false
+    enabled = false
 }
