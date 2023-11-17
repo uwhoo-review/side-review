9737f37ec21bc7450f9e350680005bb6f283dbb8
yekim <duddms9811@naver.com>
2023-11-17 16:43:24 +0900

    /contents sort와 tab 파라미터를 nullable로 변경. 둘 중 하나만 있으면 작동하도록 수정.

--- BE/src/main/kotlin/com/sideReview/side/controller/MainContentsController.kt
+++ BE/src/main/kotlin/com/sideReview/side/controller/MainContentsController.kt
@@ -4,6 +4,7 @@ import com.sideReview.side.openSearch.OpenSearchGetService
 import com.sideReview.side.openSearch.dto.MainContentDto
 import kotlinx.coroutines.runBlocking
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -16,10 +17,10 @@ class MainContentsController @Autowired constructor(
 
     @GetMapping("/contents")
     fun getContents(
-        @RequestParam(defaultValue = "main") tab: String,
+        @RequestParam(required = false) tab: String?,
         @RequestParam(required = false) sort: String?
     ): ResponseEntity<Any> {
-        val response: ResponseEntity<Any>;
+        var response: ResponseEntity<Any> = ResponseEntity(HttpStatus.BAD_REQUEST)
         runBlocking {
 
             when (tab) {
@@ -50,8 +51,13 @@ class MainContentsController @Autowired constructor(
                             openSearchGetService.get(tab, "new")
                         )
                     )
-
-                else -> response = ResponseEntity.ok("null")
+            }
+            if (tab.isNullOrBlank() && !sort.isNullOrBlank()) {
+                response = ResponseEntity.ok(
+                    openSearchGetService.parseToContent(
+                        openSearchGetService.get("sort", sort)
+                    )
+                )
             }
         }
         return response
