441b03bc74f8fa49db06d9837a9528552beb0212
chikoogi <ehthfdhkd@gmail.com>
2023-12-14 14:54:51 +0900

    [New Feature] 1. api baseurl 수정

--- FE/src/common/axios/AxiosInstance.tsx
+++ FE/src/common/axios/AxiosInstance.tsx
@@ -3,7 +3,8 @@ import { useEffect, useState } from "react";
 
 export const axiosBaseInstance = axios.create({
   // baseURL: "https://api.themoviedb.org/3",
-  baseURL: "https://15.164.189.220:443",
+  // baseURL: "https://15.164.189.220:443",
+  baseURL: "https://uwhoo-review.site/api",
   headers: {
     // Authorization:
     //   "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OWJjZmNhOWZkNWY0NGQyMjZhYzgzMTU5NzZhY2ZkYyIsInN1YiI6IjY1MWUzMmE0M2QzNTU3MDExY2ZmZThhYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-uufjYqYRvt2OC66c1euvWmZdEOGy-gGvmRoQ1fP2AA",
