3ab87aa2522ac9cf3ec7c76e54c24b05197087e7
yekim <duddms9811@naver.com>
2023-12-15 10:44:31 +0900

    테스트 커밋

---
+++ BE/src/main/kotlin/com/sideReview/side/login/naver/NaverClient.kt
@@ -0,0 +1,8 @@
+package com.sideReview.side.login.naver
+
+import org.springframework.cloud.openfeign.FeignClient
+
+@FeignClient(name = "naver", url = "https://nid.naver.com/oauth2.0/authorize")
+interface NaverClient {
+
+}
