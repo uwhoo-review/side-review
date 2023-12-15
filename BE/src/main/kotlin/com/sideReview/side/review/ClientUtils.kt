efa76ca7dd560d50b7da691fb9c8d0a64458c7d8
yekim <duddms9811@naver.com>
2023-12-15 14:52:03 +0900

    ip 여러개면 맨 앞에 하나만 쓰도록 수정

--- BE/src/main/kotlin/com/sideReview/side/review/ClientUtils.kt
+++ BE/src/main/kotlin/com/sideReview/side/review/ClientUtils.kt
@@ -9,28 +9,25 @@ class ClientUtils {
             val logger = LoggerFactory.getLogger(this.javaClass)!!
 
             var ip = request.getHeader("X-FORWARDED-FOR")
-            logger.info("X-FORWARDED-FOR : $ip")
+
             //proxy 환경일 경우
             if (ip.isNullOrBlank()) {
                 ip = request.getHeader("Proxy-Client-IP")
             }
-            logger.info("Proxy-Client-IP: $ip")
 
             //웹로직 서버일 경우
             if (ip.isNullOrBlank()) {
                 ip = request.getHeader("WL-Proxy-Client-IP")
             }
-            logger.info("WL-Proxy-Client-IP: $ip")
 
 
             if (ip.isNullOrBlank()) {
                 ip = request.remoteAddr
             }
-            logger.info("remoteAddr: $ip")
 
-            logger.error("************** review 추가 ip log ***************")
-            logger.error(ip)
-            return ip
+            val regex = """[^\d.](?:.*)""".toRegex()
+
+            return ip.replace(regex, "")
         }
     }
 }
--- BE/src/test/kotlin/com/sideReview/side/SideApplicationTests.kt
+++ BE/src/test/kotlin/com/sideReview/side/SideApplicationTests.kt
@@ -1,13 +1,16 @@
 package com.sideReview.side
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class SideApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+        val regex = """[^\d.](?:.*)""".toRegex()
+        val testS = "192.1.1.548, 654.1.81.ww3, !$@9s8,6813.23CS"
+        println(regex.find(testS))
+        println(testS.matches(regex))
+        println(testS.replace(regex, ""))
+    }
 
 }
