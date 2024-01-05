48487f5ca8efee1ce102feaab461dc93ac1cd24a
yekim <duddms9811@naver.com>
2024-01-05 17:42:42 +0900

    login : error 처리 변경2

--- BE/src/main/kotlin/com/sideReview/side/common/config/LoginErrorConfig.kt
+++ BE/src/main/kotlin/com/sideReview/side/common/config/LoginErrorConfig.kt
@@ -2,6 +2,7 @@ package com.sideReview.side.common.config
 
 import feign.FeignException
 import io.ktor.utils.io.errors.*
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import javax.servlet.http.HttpServletResponse
@@ -9,10 +10,11 @@ import javax.servlet.http.HttpServletResponse
 
 @RestControllerAdvice
 class LoginErrorConfig {
+    private val logger = LoggerFactory.getLogger(this.javaClass)!!
 
     @ExceptionHandler(FeignException::class)
     fun handleFeignStatusException(e: FeignException, response: HttpServletResponse): Exception {
-        e.printStackTrace()
+        logger.error(e.stackTraceToString())
         return Exception(e)
     }
 }
