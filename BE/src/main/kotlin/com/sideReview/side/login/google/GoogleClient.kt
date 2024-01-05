1a62cec70142a29949110cb4f624fbf1d5b19a69
yekim <duddms9811@naver.com>
2024-01-05 16:17:48 +0900

    client url 빼먹은거 추가

--- BE/src/main/kotlin/com/sideReview/side/login/google/GoogleClient.kt
+++ BE/src/main/kotlin/com/sideReview/side/login/google/GoogleClient.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 
 
-@FeignClient(name = "google")
+@FeignClient(name = "google", url = "https://www.googleapis.com")
 interface GoogleClient {
 
     @GetMapping("/userinfo/v2/me")
