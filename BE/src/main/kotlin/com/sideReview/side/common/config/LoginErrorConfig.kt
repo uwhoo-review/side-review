47dd80ce0a2c4e15ee3e9f502c5c9df2e922140d
yekim <duddms9811@naver.com>
2024-01-05 17:31:29 +0900

    login : feignclient exception 추가

---
+++ BE/src/main/kotlin/com/sideReview/side/common/config/LoginErrorConfig.kt
@@ -0,0 +1,18 @@
+package com.sideReview.side.common.config
+
+import feign.FeignException
+import io.ktor.utils.io.errors.*
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import javax.servlet.http.HttpServletResponse
+
+
+@RestControllerAdvice
+class LoginErrorConfig {
+
+    @ExceptionHandler(FeignException::class)
+    fun handleFeignStatusException(e: FeignException, response: HttpServletResponse): String? {
+        e.printStackTrace()
+        return e.message
+    }
+}
