f615dc4e981c2a336634157bfa92c65dfb9144a0
hyejin_kim <hyejin_kim@tmax.co.kr>
2024-01-10 15:41:01 +0900

    [Refactor] 리소스 경로 변경 테스트

--- BE/src/main/kotlin/com/sideReview/side/controller/TestController.kt
+++ BE/src/main/kotlin/com/sideReview/side/controller/TestController.kt
@@ -1,5 +1,6 @@
 package com.sideReview.side.controller
 
+import com.sideReview.side.login.NicknameService
 import com.sideReview.side.tmdb.TmdbContentService
 import com.sideReview.side.tmdb.TmdbPersonService
 import org.springframework.http.ResponseEntity
@@ -8,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 @RestController
 class TestController (
-    private val tmdbContentService: TmdbContentService, private val tmdbPersonService: TmdbPersonService){
+    private val tmdbContentService: TmdbContentService,
+    private val tmdbPersonService: TmdbPersonService,
+    private val nicknameService: NicknameService){
     @GetMapping("/init")
     fun getTmdb(): ResponseEntity<Any> {
         return ResponseEntity.ok(tmdbContentService.getMoreInfo(tmdbContentService.getAllContents()));
@@ -21,6 +24,6 @@ class TestController (
 
     @GetMapping("/test")
     fun getTest(): ResponseEntity<Any> {
-        return ResponseEntity.ok("ok");
+        return ResponseEntity.ok(nicknameService.makeNickname(0));
     }
 }
