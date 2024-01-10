640e5195f6ba356964a01dca784e19d4701f7efd
yekim <duddms9811@naver.com>
2024-01-10 17:04:55 +0900

    login : google client id, secret 변경

--- BE/src/main/kotlin/com/sideReview/side/login/google/dto/GoogleRequest.kt
+++ BE/src/main/kotlin/com/sideReview/side/login/google/dto/GoogleRequest.kt
@@ -3,8 +3,8 @@ package com.sideReview.side.login.google.dto
 data class GoogleRequest(
     val code:String,
     val redirect_uri:String,
-    val client_id:String = "866692341598-qivtd4h8qr8qklciift1e3b1ha8e0fao.apps.googleusercontent.com",
-    val client_secret:String = "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",
+    val client_id:String = "866692341598-2ng40irpued2c75s4vnt3cnrljc8oh9k.apps.googleusercontent.com",
+    val client_secret:String = "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",
     val grant_type:String = "authorization_code"
 
 )
