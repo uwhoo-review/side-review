215365becc1a00b96a0916f514288bd0fcbeacb8
yekim <duddms9811@naver.com>
2024-02-02 10:03:24 +0900

    로그인 security oauth2로 변경 1차 테스트

---
+++ BE/src/main/kotlin/com/sideReview/side/login/Oauth2UserServiceImpl.kt
@@ -0,0 +1,36 @@
+package com.sideReview.side.login
+
+import com.sideReview.side.common.entity.UserInfo
+import com.sideReview.side.common.repository.UserInfoRepository
+import com.sideReview.side.login.naver.OAuthAttributes
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest
+import org.springframework.security.oauth2.core.user.OAuth2User
+import org.springframework.stereotype.Service
+import kotlin.jvm.optionals.getOrNull
+
+@Service
+class Oauth2UserServiceImpl(val userInfoRepository: UserInfoRepository) :
+    DefaultOAuth2UserService() {
+
+    override fun loadUser(userRequest: OAuth2UserRequest): OAuth2User {
+        // 사용자 정보를 가져오는 로직을 여기에 구현
+        // 예시: GitHub에서 사용자 정보를 가져올 때
+        val oAuth2User = super.loadUser(userRequest)
+
+        val registrationId: String = userRequest.clientRegistration.registrationId
+        val userNameAttributeName: String =
+            userRequest.clientRegistration.providerDetails.userInfoEndpoint.userNameAttributeName
+        val attributes =
+            OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.attributes)
+        val user: UserInfo = saveOrPass(attributes)
+        oAuth2User.attributes["userInfo"] = user
+        return oAuth2User
+    }
+
+    private fun saveOrPass(attributes: OAuthAttributes): UserInfo {
+        return userInfoRepository.findById(attributes.id).getOrNull()
+            ?: userInfoRepository.save(attributes.toEntity())
+    }
+
+}
---
+++ BE/src/main/kotlin/com/sideReview/side/login/naver/OAuthAttributes.kt
@@ -0,0 +1,132 @@
+package com.sideReview.side.login.naver
+
+import com.sideReview.side.common.entity.UserInfo
+import lombok.Getter
+import javax.security.auth.login.LoginException
+
+
+@Getter
+class OAuthAttributes(
+    private val attributes: Map<String, Any>,
+    private val nameAttributeKey: String,
+    private val loginType: String,
+    val id: String,
+    val name: String,
+) {
+    companion object {
+        fun of(
+            registrationType: String,
+            userNameAttributeName: String,
+            attributes: Map<String, Any>
+        ): OAuthAttributes {
+            return when (registrationType) {
+                "google" ->
+                    ofGoogle(userNameAttributeName, attributes, registrationType)
+
+                "naver" ->
+                    ofNaver(userNameAttributeName, attributes, registrationType)
+
+                "kakao" ->
+                    ofKakao(userNameAttributeName, attributes, registrationType)
+
+                else -> throw LoginException("login registration type is invalid")
+            }
+
+        }
+
+        private fun ofKakao(
+            userNameAttributeName: String,
+            attributes: Map<String, Any>,
+            registrationType: String
+        ): OAuthAttributes {
+            val response = attributes["response"] as Map<*, *>
+            return OAuthAttributesBuilder()
+                .id(response["id"] as String)
+                .name(response["name"] as String)
+                .loginType(registrationType)
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build()
+        }
+
+        private fun ofNaver(
+            userNameAttributeName: String,
+            attributes: Map<String, Any>,
+            registrationType: String
+        ): OAuthAttributes {
+            return OAuthAttributesBuilder()
+                .id(attributes["id"] as String)
+                .name(attributes["name"] as String)
+                .loginType(registrationType)
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build()
+        }
+
+        private fun ofGoogle(
+            userNameAttributeName: String,
+            attributes: Map<String, Any>,
+            registrationType: String
+
+        ): OAuthAttributes {
+            return OAuthAttributesBuilder()
+                .id(attributes["id"] as String)
+                .name(attributes["name"] as String)
+                .loginType(registrationType)
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build()
+        }
+    }
+
+    fun toEntity(): UserInfo {
+        return UserInfoBuilder()
+            .id(id)
+            .name(name)
+            .loginType(loginType)
+            .build()
+    }
+}
+
+class OAuthAttributesBuilder {
+    private var attributes: Map<String, Any> = emptyMap()
+    private var nameAttributeKey: String = ""
+    private var id: String = ""
+    private var name: String = ""
+    private var loginType: String = ""
+
+    fun attributes(attributes: Map<String, Any>) = apply { this.attributes = attributes }
+    fun nameAttributeKey(nameAttributeKey: String) =
+        apply { this.nameAttributeKey = nameAttributeKey }
+
+    fun id(id: String) = apply { this.id = id }
+    fun name(name: String) = apply { this.name = name }
+    fun loginType(loginType: String) = apply { this.loginType = loginType }
+    fun build(): OAuthAttributes {
+        return OAuthAttributes(attributes, nameAttributeKey, loginType, id, name)
+    }
+}
+
+class UserInfoBuilder {
+    private var id: String = ""
+    private var name: String = ""
+    private var loginType: String = ""
+//    private var role: Role = Role.GUEST
+
+    fun id(id: String) = apply { this.id = id }
+    fun name(name: String) = apply { this.name = name }
+    fun loginType(loginType: String) = apply { this.loginType = loginType }
+//    fun role(role: Role) = apply { this.role = role }
+
+    fun build(): UserInfo {
+        return UserInfo(
+            userId = id,
+            loginType = loginType,
+            nickname = name,
+            preferOtt = null,
+            preferGenre = null,
+            favoriteContent = null,
+            favoritePerson = null
+        )
+    }
+}
