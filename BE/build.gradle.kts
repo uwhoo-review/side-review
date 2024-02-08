cf98ae4fdb10261a957184faf241dd6976f64d7e
yekim <duddms9811@naver.com>
2024-02-08 11:08:19 +0900

    java class를 못찾아서 시reflect 버전 명

--- BE/build.gradle.kts
+++ BE/build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.0")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
