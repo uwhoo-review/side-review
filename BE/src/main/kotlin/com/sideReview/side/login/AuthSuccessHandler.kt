6fb8e5d4ed1b4c472b58bbf7d18c53cef4a5ab77
yekim <duddms9811@naver.com>
2024-02-08 13:54:25 +0900

    로그인 : success redirect 위치 request uri로 변경

--- BE/src/main/kotlin/com/sideReview/side/login/AuthSuccessHandler.kt
+++ BE/src/main/kotlin/com/sideReview/side/login/AuthSuccessHandler.kt
@@ -1,14 +1,10 @@
 package com.sideReview.side.login
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.sideReview.side.common.entity.UserInfo
 import com.sideReview.side.common.util.MapperUtils
-import org.springframework.http.MediaType
 import org.springframework.security.core.Authentication
-import org.springframework.security.web.DefaultRedirectStrategy
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler
 import org.springframework.stereotype.Component
-import java.io.PrintWriter
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
@@ -22,7 +18,8 @@ class AuthSuccessHandler : AuthenticationSuccessHandler {
         val principal = authentication.principal as CustomOAuth2User
         val userInfoDto =
             MapperUtils.mapUserInfoToLoginResponseDto(principal.attributes["user"] as UserInfo)
+        val targetUrl = request.requestURI
         response.addHeader("userId", userInfoDto.id)
-        response.sendRedirect("https://www.uwhoo-review.site/login/redirect")
+        response.sendRedirect("${targetUrl}/login/redirect")
     }
 }
