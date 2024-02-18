0a37296c96d4f92a300a5602d2aae6a63096fedd
chikoogi <ehthfdhkd@gmail.com>
2024-02-18 17:03:34 +0900

    [New Feature] - mypage ott api 적용

--- FE/src/component/molecules/AccordionSubscribe/AccordionSubscribe.tsx
+++ FE/src/component/molecules/AccordionSubscribe/AccordionSubscribe.tsx
@@ -6,14 +6,15 @@ import HWAvatar from "@src/component/atoms/HWAvatar/HWAvatar";
 import { IconApple, IconDisney, IconNetflix, IconWatcha, IconWavve } from "@res/index";
 import { PLATFORM_ID_NAME, PLATFORM_NAME } from "@src/variables/CommonConstants";
 import { PlatformConstants } from "@src/variables/PlatformConstants";
+import { UWAxios } from "@src/common/axios/AxiosConfig";
 
 const AccordionSubscribe = ({ ott }: { ott: number[] }) => {
   const [open, setOpen] = useState(true);
   const [subscribe, setSubscribe] = useState<number[]>(ott);
 
   useEffect(() => {
-    console.log(subscribe);
-  }, [subscribe]);
+    setSubscribe(ott);
+  }, [ott]);
 
   return (
     <MenuAccordion
@@ -39,12 +40,15 @@ const AccordionSubscribe = ({ ott }: { ott: number[] }) => {
             size={"80px"}
             customCss={styled.avatar}
             active={subscribe.includes(v.id)}
-            onClick={() => {
+            onClick={async () => {
+              let tmpOtt = [];
               if (subscribe.includes(v.id)) {
-                setSubscribe(subscribe.filter((s) => s !== v.id));
+                tmpOtt = subscribe.filter((s) => s !== v.id);
               } else {
-                setSubscribe((prev) => [...prev, v.id]);
+                tmpOtt = [...subscribe, v.id];
               }
+              setSubscribe(tmpOtt);
+              const res = await UWAxios.user.putMyOtt(tmpOtt);
             }}
           >
             <v.icon />
