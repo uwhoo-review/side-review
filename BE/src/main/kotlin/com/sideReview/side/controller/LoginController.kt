e3be962124d817fcc86e2013471f5b238d84885a
yekim <duddms9811@naver.com>
2024-02-27 14:07:14 +0900

    [Edit] /user/ott ott토글 api put으로 변경

--- BE/src/main/kotlin/com/sideReview/side/controller/LoginController.kt
+++ BE/src/main/kotlin/com/sideReview/side/controller/LoginController.kt
@@ -16,6 +16,7 @@ import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.session.web.http.DefaultCookieSerializer
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import javax.servlet.http.Cookie
@@ -109,7 +110,7 @@ class LoginController(
         }
     }
 
-    @GetMapping("/user/ott/{toggle}")
+    @PutMapping("/user/ott/{toggle}")
     fun changeToggle(
         @PathVariable toggle: Boolean,
         @LoginUser user: UserInfoDto
