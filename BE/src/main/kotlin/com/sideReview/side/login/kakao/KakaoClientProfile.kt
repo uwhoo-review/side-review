b28fa619ac5e45db8a4bec64246bdf464d2e576b
yekim <duddms9811@naver.com>
2024-02-29 13:26:57 +0900

    [Edit] import 문 정리

--- BE/src/main/kotlin/com/sideReview/side/login/kakao/KakaoClientProfile.kt
+++ BE/src/main/kotlin/com/sideReview/side/login/kakao/KakaoClientProfile.kt
@@ -1,14 +1,10 @@
 package com.sideReview.side.login.kakao
 
-import com.sideReview.side.login.kakao.dto.KakaoAuthResponse
 import com.sideReview.side.login.kakao.dto.KakaoProfileResponse
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestHeader
-import org.springframework.web.bind.annotation.RequestParam
 
-//@Headers("Content-type: application/x-www-form-urlencoded")
 @FeignClient(name = "kakaoProfile", url = "https://kapi.kakao.com")
 interface KakaoClientProfile {
     @GetMapping("/v2/user/me")
