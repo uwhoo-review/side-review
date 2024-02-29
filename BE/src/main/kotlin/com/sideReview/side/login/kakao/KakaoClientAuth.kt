5f93e24684dc1dd81c3a6f0697f1675237c61dc2
yekim <duddms9811@naver.com>
2024-02-29 13:26:23 +0900

    [Edit] auth에 content type 헤더 추가

--- BE/src/main/kotlin/com/sideReview/side/login/kakao/KakaoClientAuth.kt
+++ BE/src/main/kotlin/com/sideReview/side/login/kakao/KakaoClientAuth.kt
@@ -1,14 +1,12 @@
 package com.sideReview.side.login.kakao
 
 import com.sideReview.side.login.kakao.dto.KakaoAuthResponse
-import com.sideReview.side.login.kakao.dto.KakaoProfileResponse
+import feign.Headers
 import org.springframework.cloud.openfeign.FeignClient
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestParam
 
-//@Headers("Content-type: application/x-www-form-urlencoded")
+@Headers("Content-type: application/x-www-form-urlencoded")
 @FeignClient(name = "kakaoAuth", url = "https://kauth.kakao.com")
 interface KakaoClientAuth {
 
