803e299e154e901e9f00614c5b50c4aa1cfb2fa4
yekim <duddms9811@naver.com>
2024-03-27 17:04:40 +0900

    [Refactoring] Dto 주석 제거 - 삭제한 dto의 import문 제거

--- BE/src/main/kotlin/com/sideReview/side/login/naver/NaverClientAuth.kt
+++ BE/src/main/kotlin/com/sideReview/side/login/naver/NaverClientAuth.kt
@@ -1,7 +1,6 @@
 package com.sideReview.side.login.naver
 
 import com.sideReview.side.login.naver.dto.NaverAuthResponse
-import com.sideReview.side.login.naver.dto.NaverLogoutResponse
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
