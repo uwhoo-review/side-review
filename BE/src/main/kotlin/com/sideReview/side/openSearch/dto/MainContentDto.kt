230a81794ed9ad3e609d4921be1d4ac2ea8fd391
yekim <duddms9811@naver.com>
2024-03-27 14:15:27 +0900

    [Edit] 메인 탭 4개 인생작 추가 시 dto null safety 기능 해제

--- BE/src/main/kotlin/com/sideReview/side/openSearch/dto/MainContentDto.kt
+++ BE/src/main/kotlin/com/sideReview/side/openSearch/dto/MainContentDto.kt
@@ -1,18 +1,24 @@
 package com.sideReview.side.openSearch.dto
 
+import com.fasterxml.jackson.annotation.JsonInclude
+
 data class MainContentDto(
     val popular: List<ContentDto>,
     val latest: List<ContentDto>,
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     val favorite: MainContentUserFavorite
 )
 
 // 로그인 한 유저의 경우 인생작 id를 리스트로 추가
 data class MainContentUserFavorite(
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     val popular: List<String>? = null,
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     val latest: List<String>? = null
 )
 
 data class MainPopDto(
     val content: List<ContentDto>,
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     val favorite: List<String>?
 )
