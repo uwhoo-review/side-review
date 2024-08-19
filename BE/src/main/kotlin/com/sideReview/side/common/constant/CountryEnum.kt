1d123008a8d7cf6bb071c6a5a2caf6bf96d59aef
mae <ovoa22@gmail.com>
2024-08-19 14:25:13 +0900

    수집 국가 축소

--- BE/src/main/kotlin/com/sideReview/side/common/constant/CountryEnum.kt
+++ BE/src/main/kotlin/com/sideReview/side/common/constant/CountryEnum.kt
@@ -3,13 +3,13 @@ package com.sideReview.side.common.constant
 enum class CountryEnum(val ko : String) {
     KR("한국"),
     JP("일본"),
-    CN("중국"),
-    TW("대만"),
-    HK("홍콩"),
-    US("미국"),
-    GB("영국"),
-    FR("프랑스"),
-    ES("스페인");
+    US("미국");
+    //CN("중국"),
+    //TW("대만"),
+    //HK("홍콩"),
+    //GB("영국"),
+    //FR("프랑스"),
+    //ES("스페인");
 
     companion object {
         fun getCountryCodes(): List<String> {
